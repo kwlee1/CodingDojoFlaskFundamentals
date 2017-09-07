808a00177148ae9339a5f9e29718ed29d18f48d3
kwlee <keith.won.lee@gmail.com>
2017-09-07 15:09:09 -0700

    completed whats my name

---
+++ whats_my_name/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/process', methods=['POST'])
+def process():
+    print request.form['name']
+    return redirect('/')
+
+app.run(debug=True)
