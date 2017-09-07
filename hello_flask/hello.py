43f8683c738a015531cd8a3df49d52602a7a54e0
kwlee <keith.won.lee@gmail.com>
2017-09-07 12:03:13 -0700

    completed portfolio

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
-@app.route('/success')
 def hello_world():
     return render_template('index.html')
+@app.route('/success')
 def success():
     return render_template('success.html')
 app.run(debug=True)
